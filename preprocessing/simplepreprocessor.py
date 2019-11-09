0600974b51cd3bec2337bfd6bb385df18a92768f
Sayan Pramanik <41745417+sayanpr8175@users.noreply.github.com>
2019-11-09 07:46:41 +0530

    Add files via upload

---
+++ preprocessing/simplepreprocessor.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 31 06:56:16 2019
+
+@author: sayan
+"""
+
+import cv2
+
+class SimplePreprocessor:
+    def __init__(self,width, height, inter = cv2.INTER_AREA):
+        
+        self.width = width
+        self.height = height
+        self.inter = inter
+        
+    def preprocess(self, image):
+        
+        return cv2.resize(image,  (self.width, self.height), interpolation=self.inter)
+    
